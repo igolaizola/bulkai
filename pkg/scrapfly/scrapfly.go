3033a77d6d9bd8957ec85874ad9e6ecc7dc79a66
igolaizola <11333576+igolaizola@users.noreply.github.com>
2024-01-19 21:20:56 +0100

    Fix obtaining headers and JA3 at create-session
    Adjust how scrapfly is used to obtain the JA3 fingerprint and headers
    for the session. This is done by using their websites instead of their
    API.

--- pkg/scrapfly/scrapfly.go
+++ pkg/scrapfly/scrapfly.go
@@ -3,8 +3,12 @@ package scrapfly
 const (
 	// JA3URL is the URL to the JA3 API
 	FPJA3URL = "https://tools.scrapfly.io/api/fp/ja3"
+	// JA3WebURL is the URL to the web JA3 tool
+	FPJA3WebURL = "https://scrapfly.io/web-scraping-tools/ja3-fingerprint"
 	// FPAkamaiURL is the URL to the HTTP2 API
 	FPAkamaiURL = "https://tools.scrapfly.io/api/fp/akamai"
+	// FPHTTP2WebURL is the URL to the web HTTP2 tool
+	FPHTTP2WebURL = "https://scrapfly.io/web-scraping-tools/http2-fingerprint"
 )
 
 type FPJA3 struct {
@@ -13,5 +17,5 @@ type FPJA3 struct {
 }
 
 type InfoHTTP2 struct {
-	Headers map[string]string `json:"headers"`
+	Headers map[string][]string `json:"headers"`
 }
